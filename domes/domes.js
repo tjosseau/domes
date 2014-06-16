289cdb8ffeebd5db41ef0ffe29b5955f448795d4
tjosseau <thomas.josseau@gmail.com>
2014-06-16 21:35:41 +0100

    Added whole navigation methods to elements

--- domes/domes.js
+++ domes/domes.js
@@ -15,11 +15,8 @@ void function(root) {
     "use strict" ;
 
     var domes,
-    	options = {
-            silent : false,         // If silent, no `echo` neither `warn`.
-            strict : 0,             // Strict levels send warnings or throws exception.
-            debug : 0               // Debug levels echoes various information messages.
-        },
+        NODE_EXISTS = typeof root.Node !== 'undefined',
+        ELEMENT_NODE = 1,
 
         copy = function(context, object)
         {
@@ -31,82 +28,82 @@ void function(root) {
 
         queryTo = function(elset, query)
         {
-        	var result = document.querySelectorAll(query),
-    			rl = result.length ;
-    		elset.length = rl ;
-    		while(rl--) elset[rl] = result[rl] ;
-    		elset.query = query ;
+            var result = document.querySelectorAll(query),
+                rl = result.length ;
+            elset.length = rl ;
+            while(rl--) elset[rl] = result[rl] ;
+            elset.query = query ;
 
-    		return elset ;
+            return elset ;
         } ;
 
     var DOMElementSet = function()
     {
-    	this.length = 0 ;
-    	this.query = "" ;
+        this.length = 0 ;
+        this.query = "" ;
     } ;
     copy(DOMElementSet, {
-    	prototype:
-    	{
-    		add : function(element)
-    		{
-                if (element && element.nodeType === Node.ELEMENT_NODE && !this.contains(element))
+        prototype:
+        {
+            add : function(element)
+            {
+                if (element && element.nodeType === ELEMENT_NODE && !this.contains(element))
                     this[this.length++] = element ;
 
-    			return this ;
-    		},
+                return this ;
+            },
 
-    		remove : function(element)
-    		{
-    			var removed = false ;
-    			for (var i=0 ; i<this.length ; i++) {
-    				if (removed) {
-    					this[i-1] = this[i] ;
-    					delete this[i] ;
-    				}
-    				else if (this[i] === element) {
-    					delete this[i] ;
+            remove : function(element)
+            {
+                var removed = false ;
+                for (var i=0 ; i<this.length ; i++) {
+                    if (removed) {
+                        this[i-1] = this[i] ;
+                        delete this[i] ;
+                    }
+                    else if (this[i] === element) {
+                        delete this[i] ;
                         this.length-- ;
-    					removed = true ;
-    				}
-    			}
+                        removed = true ;
+                    }
+                }
 
-    			return this ;
-    		},
+                return this ;
+            },
 
-    		empty : function()
-    		{
-    			while (this.length) delete this[--this.length] ;
+            empty : function()
+            {
+                while (this.length) delete this[--this.length] ;
 
-    			return this ;
-    		},
+                return this ;
+            },
 
-    		update : function()
-    		{
-    			this.empty() ;
-    			if (this.query) queryTo(this, this.query) ;
+            update : function()
+            {
+                this.empty() ;
+                if (this.query) queryTo(this, this.query) ;
 
-    			return this ;
-    		},
+                return this ;
+            },
 
-    		merge : function(elset)
-    		{
-    			var a = 0,
-    				e,
-    				el ;
-    			while (el = arguments[a]) {
-    				el = el.length ;
-    				e = 0 ;
-    				while (e < el) this.add(arguments[a++][e++]) ;
-    			}
+            merge : function(elset)
+            {
+                var a = -1,
+                    e,
+                    el ;
+                while ((el = arguments[++a])) {
+                    el = el.length ;
+                    e = 0 ;
+                    while (e < el) this.add(arguments[a][e++]) ;
+                }
 
-    			return this ;
-    		},
+                return this ;
+            },
 
-    		get : function(i)
-    		{
-    			return domes.create().add(this[i]) ;
-    		},
+            get : function(i)
+            {
+                return domes.create().add(this[i]) ;
+            },
 
             contains : function(element)
             {
@@ -117,126 +114,316 @@ void function(root) {
                 return false ;
             },
 
-    		first : function()
-    		{
-    			return this.get(0) ;
-    		},
+            first : function()
+            {
+                return this.get(0) ;
+            },
 
-    		last : function()
-    		{
-    			return this.get(this.length-1) ;
-    		},
+            last : function()
+            {
+                return this.get(this.length-1) ;
+            },
 
-    		element : function()
-    		{
-    			return this[0] ;
-    		},
+            element : function()
+            {
+                return this[0] ;
+            },
 
-    		elements : function()
-    		{
-    			var els = [],
-    				e = 0 ;
-    			while (e < this.length)
-    				els.push(this[e++]) ;
+            elements : function()
+            {
+                var els = [],
+                    e = 0 ;
+                while (e < this.length)
+                    els.push(this[e++]) ;
 
-    			return els ;
-    		},
+                return els ;
+            },
 
             parent : function()
             {
-                var parentSet = domes.create(),
+                var set = domes.create(),
                     i = 0 ;
                 while (i < this.length)
-                    parentSet.add(this[i++].parentNode) ;
+                    set.add(this[i++].parentNode) ;
 
-                return parentSet ;
+                return set ;
             },
 
             children : function()
             {
-                var childSet = domes.create(),
-                    el,
-                    i = 0 ;
-                while (i < this.length) {
-                    el = this[i++] ;
-                    for (var c = 0, cl = el.childNodes.length ; c < cl ; c++)
-                        childSet.add(el.childNodes[c]) ;
+                var set = domes.create(),
+                    c,
+                    cl,
+                    i = -1 ;
+                while (++i < this.length) {
+                    c = -1 ;
+                    cl = this[i].childNodes.length ;
+                    while (c++ < cl) set.add(this[i].childNodes[c]) ;
                 }
 
-                return childSet ;
+                return set ;
             },
 
-            next : function()
+            next : NODE_EXISTS ?
+                function()
+                {
+                    var set = domes.create(),
+                        i = -1 ;
+                    while (++i < this.length)
+                        set.add(this[i].nextElementSibling) ;
+
+                    return set ;
+                } :
+                function()
+                {
+                    var set = domes.create(),
+                        el,
+                        i = -1 ;
+                    while (++i < this.length) {
+                        el = this[i] ;
+                        do { el = el.nextSibling ; } while (el && el.nodeType !== ELEMENT_NODE) ;
+                        set.add(el) ;
+                    }
+
+                    return set ;
+                },
+
+            previous : NODE_EXISTS ?
+                function()
+                {
+                    var set = domes.create(),
+                        i = -1 ;
+                    while (++i < this.length)
+                        set.add(this[i].previousElementSibling) ;
+
+                    return set ;
+                } :
+                function()
+                {
+                    var set = domes.create(),
+                        el,
+                        i = -1 ;
+                    while (++i < this.length) {
+                        el = this[i] ;
+                        do { el = el.previousSibling ; } while (el && el.nodeType !== ELEMENT_NODE) ;
+                        set.add(el) ;
+                    }
+
+                    return set ;
+                },
+
+            type : function()
             {
-                var nextSet = domes.create(),
-                    i = 0 ;
-                while (i < this.length)
-                    nextSet.add(this[i++].nextElementSibling) ;
+                if (!this.length) return '' ;
 
-                return nextSet ;
+                var l = this.length,
+                    e,
+                    type ;
+                while (l--) {
+                    e = this[l] ;
+                    if (type && e.tagName !== type) return '*' ;
+                    else type = e.tagName ;
+                }
+                return type.toLowerCase() ;
             },
 
-            previous : function()
+            listen : function(event)
             {
-                var previousSet = domes.create(),
-                    i = 0 ;
-                while (i < this.length)
-                    previousSet.add(this[i++].previousElementSibling) ;
 
-                return previousSet ;
             },
 
-    		type : function()
-    		{
-    			if (!this.length) return '' ;
+            forget : function(event)
+            {
 
-    			var l = this.length,
-	        		e,
-	        		type ;
-	        	while (l--) {
-	        		e = this[l] ;
-	        		if (type && e.tagName !== type) return '*' ;
-	        		else type = e.tagName ;
-	        	}
-	        	return type.toLowerCase() ;
-    		},
+            },
 
-    		listen : function(event)
-    		{
+            outer : function(el)
+            {
+                var i = -1 ;
 
-    		},
+                if (!el || el === true) {
+                    if (!this.length) return null ;
+                    var htmls = [] ;
+                    while (++i < this.length)
+                        htmls.push(this[i].outerHTML) ;
+                    return el === true ? htmls : htmls.join("") ;
+                }
 
-    		forget : function(event)
-    		{
+                if (typeof el === 'string')
+                    while (++i < this.length)
+                        this[i].outerHTML = el ;
+                else {
+                    if (el.nodeType === ELEMENT_NODE)
+                        while (++i < this.length)
+                            this[i].outerHTML = el.outerHTML ;
+                    else if (el instanceof DOMElementSet)
+                        while (++i < this.length)
+                            this[i].outerHTML = el.outer() ;
+                    else
+                        while (++i < this.length)
+                            this[i].outerHTML = el.toString() ;
+                }
 
-    		}
-    	}
+                return this ;
+            },
+
+            inner : function(el)
+            {
+                var i = -1 ;
+
+                if (!el || el === true) {
+                    if (!this.length) return null ;
+                    var htmls = [] ;
+                    while (++i < this.length)
+                        htmls.push(this[i].innerHTML) ;
+                    return el === true ? htmls : htmls.join("") ;
+                }
+
+                if (typeof el === 'string')
+                    while (++i < this.length)
+                        this[i].innerHTML = el ;
+                else {
+                    if (el.nodeType === ELEMENT_NODE)
+                        while (++i < this.length)
+                            this[i].innerHTML = el.outerHTML ;
+                    else if (el instanceof DOMElementSet)
+                        while (++i < this.length)
+                            this[i].innerHTML = el.outer() ;
+                    else
+                        while (++i < this.length)
+                            this[i].innerHTML = el.toString() ;
+                }
+
+                return this ;
+            },
+
+            append : function(el)
+            {
+                if (typeof el === 'string')
+                    while (++i < this.length)
+                        this[i].insertAdjacentHTML('beforeend', el) ;
+                else {
+                    if (el.nodeType === ELEMENT_NODE)
+                        this[this.length-1].appendChild(el) ;
+                    else if (el instanceof DOMElementSet) {
+                        var target = this[this.length-1],
+                            e = -1 ;
+                        while (++e < el.length)
+                            target.appendChild(el[e]) ;
+                    }
+                }
+
+                return this ;
+            },
+
+            prepend : function(el)
+            {
+                var i = -1 ;
+
+                if (typeof el === 'string')
+                    while (++i < this.length)
+                        this[i].insertAdjacentHTML('afterbegin', el) ;
+                else {
+                    var target = this[this.length-1] ;
+                    if (el.nodeType === ELEMENT_NODE)
+                        target.insertBefore(el, target.firstChild) ;
+                    else if (el instanceof DOMElementSet) {
+                        if ((i = el.length))
+                            while (i--)
+                                target.insertBefore(el[i], target.firstChild) ;
+                    }
+                }
+
+                return this ;
+            },
+
+            before : function(el)
+            {
+                var i = -1 ;
+
+                if (typeof el === 'string')
+                    while (++i < this.length)
+                        this[i].insertAdjacentHTML('beforebegin', el) ;
+                else {
+                    var target = this[this.length-1] ;
+                    if (el.nodeType === ELEMENT_NODE)
+                        target.parentNode.insertBefore(el, target) ;
+                    else if (el instanceof DOMElementSet) {
+                        if ((i = el.length))
+                            while (i--)
+                                target.parentNode.insertBefore(el[i], target) ;
+                    }
+                }
+
+                return this ;
+            },
+
+            after : function(el)
+            {
+                var i = -1 ;
+
+                if (typeof el === 'string')
+                    while (++i < this.length)
+                        this[i].insertAdjacentHTML('afterend', el) ;
+                else {
+                    var target = this[this.length-1] ;
+                    if (el.nodeType === ELEMENT_NODE)
+                        target.parentNode.insertBefore(el, target.nextSibling) ;
+                    else if (el instanceof DOMElementSet) {
+                        if ((i = el.length))
+                            while (i--)
+                                target.parentNode.insertBefore(el[i], target.nextSibling) ;
+                    }
+                }
+
+                return this ;
+            },
+
+            text : function(str)
+            {
+                var i = -1 ;
+
+                if (typeof str === 'string')
+                    while (++i < this.length)
+                        this[i].innerText = str ;
+                else {
+                    if (!this.length) return null ;
+
+                    var texts = [] ;
+                    while (++i < this.length)
+                        texts.push(this[i].innerText) ;
+                    return str === true ? texts : texts.join("") ;
+                }
+
+                return this ;
+            }
+        }
     }) ;
 
     domes = root.domes = function() {
 
     } ;
     copy(domes, {
-    	create : function(type)
-    	{
-    		var elset = new DOMElementSet() ;
-    		if (type)
-	    		switch (type) {
-					case 'svg' :
-						elset.add(document.createElementNS("http://www.w3.org/2000/svg", type)) ;
-						break ;
+        create : function(type)
+        {
+            var elset = new DOMElementSet() ;
+            if (type)
+                switch (type) {
+                    case 'svg' :
+                        elset.add(document.createElementNS("http://www.w3.org/2000/svg", type)) ;
+                        break ;
 
-					default :
-						elset.add(document.createElement(type)) ;
-						break ;
-	    		}
-    		return elset ;
-    	},
+                    default :
+                        elset.add(document.createElement(type)) ;
+                        break ;
+                }
+            return elset ;
+        },
 
-    	query : function(query)
-    	{
-    		return queryTo(domes.create(), query) ;
-    	}
+        query : function(query)
+        {
+            return queryTo(domes.create(), query) ;
+        }
     }) ;
 
 }(window) ;
